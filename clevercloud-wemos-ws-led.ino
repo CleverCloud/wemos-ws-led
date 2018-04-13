a258a32c7f82b60d8c1cc69556fb084a4140e8c4
caillef <corentin.cailleaud@epitech.eu>
2018-04-13 17:21:32 +0200

    Add degrade

--- clevercloud-wemos-ws-led.ino
+++ clevercloud-wemos-ws-led.ino
@@ -1,5 +1,5 @@
 #include <Adafruit_NeoPixel.h>    // https://github.com/adafruit/Adafruit_NeoPixel
-#include <ESP8266WiFi.h>          
+#include <ESP8266WiFi.h>
 #include <WebSocketClient.h>      // https://github.com/morrissinger/ESP8266-Websocket
 
 #define PIXEL_COUNT       30
@@ -16,19 +16,22 @@ struct RGB {
 };
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, D4, NEO_GRB + NEO_KHZ800);
-RGB stripView[PIXEL_COUNT];
 WebSocketClient wsClient;
 WiFiClient client;
+
+RGB color;
+RGB lastColor;
+int scrolling;
 
 bool connected;
 
 void setup() {
   Serial.begin(9600); // Used for debugging messages
 
-  // Initialize the LED strip
-  for (int i = 0; i < strip.numPixels(); i = i + 1) {
-    stripView[i] = {0, 0, 0};
-  }
+  // Initialize the colors
+  lastColor = {0, 0, 0};
+  color = {0, 0, 0};
+  scrolling = 0;
   strip.begin();
   strip.show();
 
@@ -38,20 +41,22 @@ void setup() {
 
 void loop() {
   // If connection fails / breaks, the first LED blinks red
-  if(!client.connected() || !connected) {
-    stripView[0] = {255, 0, 0};
-    renderStrip();
+  if (!client.connected() || !connected) {
+    renderLEDColor(0, {255, 0, 0});
     delay(500);
-    stripView[0] = {0, 0, 0};
-    renderStrip();
+    renderLEDColor(0, {0, 0, 0});
     delay(500);
+  } else if (scrolling > 0) { // If scrolling is greater than 0, render LEDs
+    renderLED(strip.numPixels() - scrolling);
+    delay(20);
+    scrolling--;
   } else {
-    // When receiving data, process it and display the corresponding color on every LED
+    // When receiving data, process it and start scrolling animation
     // Expected format is: RRRGGGBBB (as in 255000000, 035127078, ...)
     // There is no error handling
     String data;
     wsClient.getData(data);
-    if(data.length() > 0) {
+    if (data.length() > 0) {
       Serial.print("Received: ");
       Serial.println(data);
       int red   = data.substring(0, 3).toInt() / 255.0 * 100;
@@ -63,10 +68,13 @@ void loop() {
       Serial.println(green);
       Serial.print("Blue: ");
       Serial.println(blue);
-      for (int i = 0; i < strip.numPixels(); i = i + 1) {
-        stripView[i] = {red, green, blue};
-      }
-      renderStrip();
+
+      // Update colors
+      lastColor = color;
+      color = {red, green, blue};
+
+      // Start scrolling animation
+      scrolling = strip.numPixels();
     }
   }
 }
@@ -112,10 +120,24 @@ void setup_websocket() {
   }
 }
 
-void renderStrip() {
-  for (int i = 0; i < strip.numPixels(); i = i + 1) {
-    strip.setPixelColor(i, stripView[i].r, stripView[i].g, stripView[i].b);
-  }
+double diffAbs(byte last, byte current) {
+  return (double) (last > current ? last - current : current - last);
+}
+
+byte computeValueAt(byte last, byte current, int i) {
+  double ratio = (double) i / (double) strip.numPixels();
+  return (current + (last > current ? 1 : -1) * (byte) (diffAbs(last, current) * ratio));
+}
+
+void renderLED(int i) {
+  byte r = computeValueAt(lastColor.r, color.r, i);
+  byte g = computeValueAt(lastColor.g, color.g, i);
+  byte b = computeValueAt(lastColor.b, color.b, i);
+  strip.setPixelColor(i, r, g, b);
   strip.show();
 }
 
+void renderLEDColor(int i, RGB color) {
+  strip.setPixelColor(i, color.r, color.g, color.b);
+  strip.show();
+}
